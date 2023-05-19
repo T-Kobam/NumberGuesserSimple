e1d39fdd1c5bdd9d4f8bc5c3ad779bdc029f475e
kobam <takuya.2441okc@gmail.com>
2023-05-20 01:07:43 +0900

    game画面作成

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -7,19 +8,30 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   correctNumber: number;
-  guessedNumber: number;
-  guessingCount: number;
   message: string;
+  errorMessage: string;
 
-  constructor() {
+  constructor(private router: Router) {
     this.correctNumber = 0;
-    this.guessedNumber = 0;
-    this.guessingCount = 0;
-    this.message = "Player one enter a value between 1 and 100 to be guessed";
+    this.message = "プレイヤー1は1から100までの整数値を1つ入力してください";
+    this.errorMessage = "";
   }
 
-  onkeydown(event: any): void {
-    this.correctNumber = event.target.value;
-    this.message = `${this.correctNumber} & enter clicked!!`;
+  onKeyDown(event: any): void {
+    const value: number = event.target.value;
+    this.errorMessage = "";
+    // 整数値の検証
+    if (isNaN(Number(value))) {
+      this.errorMessage = "整数値を入力してください";
+      return;
+    }
+    // 1 <= value <= 100の検証
+    if (value < 1 || value >100) {
+      this.errorMessage = "1から100までの整数値を1つ入力してください";
+      return;
+    } 
+
+    this.correctNumber = value;
+    this.router.navigate(["/guess"]);
   }
 }
