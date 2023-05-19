d448e38e585547a6e0963b016564fadc191326f3
kobam <takuya.2441okc@gmail.com>
2023-05-20 00:03:29 +0900

    FormsModule追加(Form機能を使用するため)

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent {
+  correctNumber: number;
+  guessedNumber: number;
+  guessingCount: number;
+  message: string;
 
+  constructor() {
+    this.correctNumber = 0;
+    this.guessedNumber = 0;
+    this.guessingCount = 0;
+    this.message = "Player one enter a value between 1 and 100 to be guessed";
+  }
+
+  onkeydown(event: any): void {
+    this.correctNumber = event.target.value;
+    this.message = `${this.correctNumber} & enter clicked!!`;
+  }
 }
